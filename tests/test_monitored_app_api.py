6777a64b53e3c5cc96f02a209d71988da4d6cf44
Konstantin <doom2good@mail.ru>
2022-12-22 20:20:56 +0300

    рефакторинг, тесты

--- tests/test_monitored_app_api.py
+++ tests/test_monitored_app_api.py
@@ -157,34 +157,3 @@ def _get_test_logs(app_info, count, days=120):
         logs.append(data)
 
     return logs
-
-# def test_registration(client, app):
-#     response = client.post("/register", data={"email": "test@test.com", "password": "testpassword"})
-#
-#     with app.app_context():
-#         assert User.query.count() == 1
-#         assert User.query.first().email == "test@test.com"
-#
-
-# @responses.activate
-# def test_age(client):
-#     responses.add(
-#         responses.GET,
-#         "https://api.agify.io",
-#         json={"age": 33, "count": 1049384, "name": "Anthony"},
-#         status=200
-#     )
-#     client.post("/register", data={"email": "test@test.com", "password": "testpassword"})
-#     client.post("/login", data={"email": "test@test.com", "password": "testpassword"})
-#
-#     response = client.post("/age", data={"name": "Anthony"})
-#
-#     assert b"You are 33 years old" in response.data
-#
-#
-# def test_invalid_login(client):
-#     client.post("/login", data={"email": "test@test.com", "password": "testpassword"})
-#
-#     response = client.get("/city")
-#
-#     assert response.status_code == 401
